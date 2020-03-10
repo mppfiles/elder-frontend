90a62e721ed0d952034104dac9da1d44d053bc5c
mppfiles <mppfiles@gmail.com>
2020-03-10 14:04:48 -0300

    agrego flag para login

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ app.ajax = {
 
             bootbox.hideAll();
 
-            if (xhr.status === 403) {
+            if (xhr.status === 403 || xhr.responseText.toLowerCase().includes("login")) {
                 app.ui.mensajeError(app.ajax.messages.access_denied_title, app.ajax.messages.access_denied);
                 return;
             }
